1fcd65f2a7491c433fce205075b6d2910680c682
The X <haroune.official2@gmail.com>
2022-02-16 14:19:26 +0100

    finish: emerald icon button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,10 @@
 
+const buttonStyle = `inline-flex flex-row items-center 
+            bg-primary hover:bg-primary-dark disabled:bg-disabled 
+            disabled:text-disabled-dark text-primary-screen 
+            rounded-lg transition-all`
+
+
 export const Button = (props) => {
 
     const isDisabled = props.isDisabled;
@@ -12,11 +18,28 @@ export const Button = (props) => {
     return (
         <button 
             disabled={isDisabled}
-            className="h-[60px] m-3 py-2 px-5 inline-flex flex-row items-center bg-primary hover:bg-primary-dark 
-            disabled:bg-disabled disabled:text-disabled-dark text-primary-screen 
-            rounded-lg transition-all"
+            className={`h-[60px] m-3 py-2 px-5 ${buttonStyle}`}
             onClick={()=> handleOnClick()}>
             {props.children}
+        </button>
+    )
+}
+
+
+export const IconifiedButton = (props) => {
+
+    function handleOnClick(){
+        if(!props.isDisabled){
+            props.onClick()
+        }
+    }
+
+    return (
+        <button 
+            className={`h-[60px] w-[60px] m-3 p-5 ${buttonStyle}`}
+            disabled={props.isDisabled}
+            onClick={handleOnClick}>
+            <ButtonIcon icon={props.icon}/>
         </button>
     )
 }
