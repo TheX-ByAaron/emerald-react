da70b8360fab152658b6850cc85161c098a20692
The X <haroune.official2@gmail.com>
2022-02-26 18:07:21 +0100

    add: top naviation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import ImageCheckBox from "../components/ImageCheckBox";
+import musicNote from "../assets/icons/eye.svg";
 import Image from "../assets/images/image.jpg";
 import { useState } from "react";
+import TopNav from "../components/TopNav";
 
 const Home = ()=>{
     const [checked, setChecked] = useState(false);
@@ -10,14 +12,29 @@ const Home = ()=>{
     }
 
     return (
-        <div className="w-full h-screen grid content-center justify-items-center 
+        <div className="w-full min-h-screen grid content-center justify-items-center 
         bg-primary-surface">
-            <ImageCheckBox 
-                src={Image} 
-                EmeraldStyle="h-[150px] w-[300px]"
-                checked={checked}
-                onCheckChange={replaceCheck}
-            />
+            <TopNav mainLogo={Image}
+                navItems={[{
+                    icon: musicNote,
+                    title: "Hello"
+                },
+                {
+                    icon: musicNote,
+                    title: "music"
+                },
+                {
+                    icon: musicNote,
+                    title: "kero"
+                },
+                {
+                    icon: musicNote,
+                    title: "more than that"
+                }]}
+                
+                onItemClick={(index) => { alert(index)}}
+                onMainLogoClick={() => alert("logo")}/>
+            
         </div>
     )
 }
