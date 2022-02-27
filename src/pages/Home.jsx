ae39c9c2a44e1dbf5d6847a6553e22569375bf05
The X <haroune.official2@gmail.com>
2022-02-27 17:32:18 +0100

    add: side navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,24 @@ import ImageCheckBox from "../components/ImageCheckBox";
 import musicNote from "../assets/icons/eye.svg";
 import Image from "../assets/images/image.jpg";
 import { useState } from "react";
-import TopNav from "../components/TopNav";
+import SideNav from "../components/SideNav";
 
 const Home = ()=>{
     const [checked, setChecked] = useState(false);
+    const [activeItem, setActiveItem] = useState(0);
 
-    function replaceCheck(){
-        setChecked(isChecked => isChecked = !checked);
+    function replaceIndex(index){
+        setActiveItem(currentIndex => currentIndex = index);
     }
 
+    let header = (
+        <img src={Image} alt="main logo" className="h-4/5 m-auto" />
+    )
+
     return (
-        <div className="w-full min-h-screen grid content-center justify-items-center 
-        bg-primary-surface">
-            <TopNav mainLogo={Image}
+        <div className="w-full min-h-screen bg-primary-screen">
+            <SideNav 
+                header={header}
                 navItems={[{
                     icon: musicNote,
                     title: "Hello"
@@ -32,8 +37,50 @@ const Home = ()=>{
                     title: "more than that"
                 }]}
                 
-                onItemClick={(index) => { alert(index)}}
-                onMainLogoClick={() => alert("logo")}/>
+                onItemClick={(index) => { replaceIndex(index)}}
+                activeItemIndex={activeItem}/>
+
+            <div className="ml-[25%] p-4">
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+
+                <ImageCheckBox 
+                checked={false}
+                src={Image}
+                EmeraldStyle="h-[200px] w-[200px]"/>
+            </div>
             
         </div>
     )
